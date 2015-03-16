e83b457be48eb916549e70b4e8ce6b520a1edf48
Sean Callan <seancallan@gmail.com>
2015-03-16 11:08:48 -0700

    Renamed Reader to Document

---
+++ lib/mits/document.rb
@@ -0,0 +1,30 @@
+require 'open-uri'
+require 'saxerator'
+
+module MITS
+  class Document
+    attr_reader :parser
+
+    def initialize(url)
+      @parser = ::Saxerator.parser(open(url)) do |c|
+        c.put_attributes_in_hash!
+        c.symbolize_keys!
+      end
+    end
+
+    def companies
+      return enum_for(:companies) unless block_given?
+
+      physical_property.within(:Management).within(:PropertyContacts).each do |tag|
+        yield Mapper.company(tag)
+      end
+    end
+
+    private
+
+    def physical_property
+      parser.within(:PhysicalProperty)
+    end
+
+  end
+end
---
+++ spec/lib/mits/document_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+module MITS
+  describe Document do
+    subject { Document.new('./spec/fixtures/mits.xml') }
+
+    describe '#properties' do
+      it 'will return the properties' do
+      end
+    end
+
+    describe '#companies' do
+      it 'will return the companies' do
+        companies = subject.companies.reduce([]) { |arr, company| arr.push(company) }
+        expect(companies.first).to be_a Company
+        expect(companies.size).to eq 1
+      end
+    end
+  end
+end
