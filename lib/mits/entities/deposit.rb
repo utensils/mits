a91e7eb19408dc80d920e355455fa92221725062
Sean Callan <seancallan@gmail.com>
2015-03-18 23:32:32 -0700

    Deposit entity

---
+++ lib/mits/entities/deposit.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+module MITS
+  class Deposit
+    include SimpleObjects::Base
+
+    attribute :amount
+    attribute :description
+    attribute :percent_refundable
+    attribute :portion_refundable
+    attribute :type
+  end
+end
