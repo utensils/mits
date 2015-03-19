26bf1b2fdb1420d0a616a4eb95968428e9c7dea3
Sean Callan <seancallan@gmail.com>
2015-03-18 23:37:02 -0700

    Property entity

---
+++ lib/mits/entities/property.rb
@@ -0,0 +1,12 @@
+module MITS
+  class Property
+    include SimpleObjects::Base
+
+    attribute :deposit
+    attribute :fees
+    attribute :id
+    attribute :name
+    attribute :units
+    attribute :website
+  end
+end
