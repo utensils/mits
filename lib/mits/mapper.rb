388c198f476495d228f0778ae792f395eb8f5fb4
Sean Callan <seancallan@gmail.com>
2015-03-19 16:49:26 -0700

    Updated mapper to support Properties

--- lib/mits/mapper.rb
+++ lib/mits/mapper.rb
@@ -7,12 +7,93 @@ module MITS
       Address.new(attrs)
     end
 
+    def amenities(tags)
+      if tags
+        tags = [tags] unless tags.is_a? Array
+        tags.map { |tag| amenity(tag) }
+      end
+    end
+
+    def amenity(tag)
+      Amenity.new(description: tag[:Description], type: tag[:AmenityType])
+    end
+
     def company(tag)
       attrs = company_attributes(tag)
       Company.new(attrs)
     end
 
+    def deposit(tag)
+      attrs = {
+        amount: deposit_amount(tag[:Amount][:ValueRange]),
+        description: tag[:Description],
+        percent_refundable: try(tag[:PercentRefundable], :to_f),
+        portion_refundable: try(tag[:PortionRefundable], :to_f),
+        type: tag[:DepositType],
+      }
+      Deposit.new(attrs)
+    end
+
+    def fees(tag)
+      attrs = {
+        admin_fee: try(tag[:AdminFee], :to_f),
+        application_fee: try(tag[:ApplicationFee], :to_f),
+        broker_fee: try(tag[:BrokerFee], :to_f),
+        late_fee_per_day: try(tag[:LateFeePerDay], :to_f),
+        late_min_fee: try(tag[:LateMinFee], :to_f),
+        late_percent: try(tag[:LatePercent], :to_f),
+        late_type: tag[:LateType],
+        non_refundable_hold_fee: try(tag[:NonRefundableHoldFee], :to_f),
+        prorate_type: tag[:ProrateType]
+      }
+      Fees.new(attrs)
+    end
+
+    def property(tag)
+      attrs = {
+        amenities: amenities(tag[:ILS_Unit][:Amenity]),
+        deposit: deposit(tag[:Deposit]),
+        fees: fees(tag[:Fee]),
+        id: tag[:IDValue],
+        name: tag[:PropertyID][:MarketingName],
+        units: units(tag[:ILS_Unit][:Units][:Unit]),
+        website: tag[:PropertyID][:WebSite]
+      }
+      Property.new(attrs)
+    end
+
+    def units(tags)
+      tags = [tags] unless tags.is_a? Array
+      tags.map do |tag|
+        attrs = {
+          bathrooms: try(tag[:UnitBathrooms], :to_f),
+          bedrooms: try(tag[:UnitBedrooms], :to_f),
+          name: tag[:MarketingName],
+          rent: try(tag[:UnitRent], :to_f),
+          sqft: unit_sqft(tag)
+        }
+        Unit.new(attrs)
+      end
+    end
+
     private
+
+    def deposit_amount(tag)
+      if tag[:Exact]
+        amount = tag[:Exact].to_f
+      else
+        min = tag[:Min].to_f
+        max = tag[:Max].to_f
+        amount = Range.new(min, max)
+      end
+      amount
+    end
+
+    def unit_sqft(tag)
+      min = tag[:MinSquareFeet]
+      max = tag[:MaxSquareFeet]
+      Range.new(min.to_i, max.to_i)
+    end
 
     def company_attributes(tag)
       {
@@ -35,6 +116,11 @@ module MITS
         postal_code: tag[:PostalCode],
         country:     tag[:Country],
       }
+    end
+
+    def try(value, method)
+      return nil unless value
+      value.send(method)
     end
 
   end
--- spec/lib/mits/mapper_spec.rb
+++ spec/lib/mits/mapper_spec.rb
@@ -2,17 +2,25 @@ require 'spec_helper'
 
 module MITS
   describe Mapper do
-    let(:address_tag) do
-      {
-        Type: 'other',
-        Description: 'desc',
-        AddressLine1: 'line1',
-        AddressLine2: 'line2',
-        City: 'city',
-        State: 'state',
-        PostalCode: 94611,
-        Country: 'US'
-      }
+    let(:fixture) { YAML.load_file('./spec/fixtures/property.yml') }
+
+    describe '.address' do
+      subject { Mapper.address(fixture[:PropertyID][:Address]) }
+
+      it { is_expected.to be_a Address }
+    end
+
+    describe '.amenities' do
+      subject { Mapper.amenities(fixture[:ILS_Unit][:Amenity]) }
+
+      it { is_expected.to be_a Array }
+
+      it 'include various amenities' do
+        first = subject.first
+        expect(first).to be_a Amenity
+        expect(first.description).to eq 'Refrigerator'
+        expect(first.type).to eq 'Refrigerator'
+      end
     end
 
     describe '.company' do
@@ -23,7 +31,7 @@ module MITS
             IDType: 'id type'
           },
           CompanyName: 'name',
-          Address: address_tag,
+          Address: fixture[:PropertyID][:Address],
           Website: 'http://...',
           Logo: 'http://...',
         }
@@ -34,10 +42,77 @@ module MITS
       it { is_expected.to be_a Company }
     end
 
-    describe '.address' do
-      subject { Mapper.address(address_tag) }
+    describe '.deposit' do
+      subject { Mapper.deposit(fixture[:Deposit]) }
 
-      it { is_expected.to be_a Address }
+      it { is_expected.to be_a Deposit }
+
+      it 'include deposit details' do
+        expect(subject.amount).to eq 3000.00
+        expect(subject.description).to be_nil
+        expect(subject.type).to eq 'Security Deposit'
+      end
+
+      context 'when a range' do
+        let(:ranged_deposit) do
+          { Amount: { ValueRange: { Min: 100, Max: 1000 } } }
+        end
+
+        subject { Mapper.deposit(ranged_deposit) }
+
+        it 'include deposit details' do
+          expect(subject.amount).to eq 100..1000
+        end
+      end
+    end
+
+    describe '.fees' do
+      subject { Mapper.fees(fixture[:Fee]) }
+
+      it { is_expected.to be_a Fees }
+
+      it 'include individual fee amounts' do
+        expect(subject.admin_fee).to eq 0.0
+        expect(subject.application_fee).to eq 50.0
+        expect(subject.broker_fee).to eq 0.0
+        expect(subject.late_fee_per_day).to eq 0.0
+        expect(subject.late_min_fee).to eq 0.0
+        expect(subject.late_percent).to eq 0.0
+        expect(subject.late_type).to eq 'Standard'
+        expect(subject.non_refundable_hold_fee).to eq 0.0
+        expect(subject.prorate_type).to eq 'Standard'
+      end
+    end
+
+    describe '.property' do
+      subject { Mapper.property(fixture) }
+
+      it { is_expected.to be_a Property }
+
+      it 'includes details' do
+        expect(subject.deposit).to be_a Deposit
+        expect(subject.fees).to be_a Fees
+        expect(subject.id).to eq 'SomethingRandom1'
+        expect(subject.name).to eq 'Cool New Apartment!!!!'
+        expect(subject.units).to be_a Array
+        expect(subject.website).to eq 'http://harris.net/juwan'
+      end
+    end
+
+    describe '.units' do
+      subject { Mapper.units(fixture[:ILS_Unit][:Units][:Unit]) }
+
+      it { is_expected.to be_a Array }
+
+      it 'includes individual units' do
+        first = subject.first
+        expect(first).to be_a Unit
+        expect(first.bathrooms).to eq 4.0
+        expect(first.bedrooms).to eq 4.0
+        expect(first.name).to eq 'Cool New Apartment!!!!'
+        expect(first.rent).to eq 3400.0
+        expect(first.sqft).to eq 3400..3400
+      end
     end
   end
 end
