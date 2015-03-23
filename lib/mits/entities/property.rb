b5d6515ce5cd840106a281fafcbb7cb5fd6c76a4
Sean Callan <seancallan@gmail.com>
2015-03-22 22:51:01 -0700

    Added Description and Summary to Property
    + Additional test coverage for Mapper#property
    + Added missing attributes from Property

--- lib/mits/entities/property.rb
+++ lib/mits/entities/property.rb
@@ -2,10 +2,14 @@ module MITS
   class Property
     include SimpleObjects::Base
 
+    attribute :amenities
     attribute :deposit
+    attribute :description
     attribute :fees
     attribute :id
     attribute :name
+    attribute :pet_policy
+    attribute :summary
     attribute :units
     attribute :website
   end
