86cb91ad1ce8a36352156e9edfc24e5e77294b88
Sean Callan <seancallan@gmail.com>
2015-07-23 10:05:47 -0700

    Default property collections

--- lib/mits/v3.0/property.rb
+++ lib/mits/v3.0/property.rb
@@ -4,20 +4,20 @@ module MITS
       include SimpleObjects::Base
 
       attribute :address
-      attribute :amenities
+      attribute :amenities, default: []
       attribute :company_id
       attribute :deposit
       attribute :description
-      attribute :fees
-      attribute :files
-      attribute :floorplans
+      attribute :fees, default: []
+      attribute :files, default: []
+      attribute :floorplans, default: []
       attribute :id
       attribute :name
-      attribute :phones
+      attribute :phones, default: []
       attribute :policy
       attribute :summary
       attribute :type
-      attribute :units
+      attribute :units, default: []
       attribute :website
     end
   end
